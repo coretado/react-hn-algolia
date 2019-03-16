13e13b81b959d4fc6260ee06ae32be5fcd091a27
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-03-15 21:37:38 -0700

    Added SVG as functions. Added initial CSS styling.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import ReactLogo from "../../icons/ReactLogo.svg";
+import styles from "./Navigation.module.css";
 
-const Navigation = () => <div>Look at me, I'm the navigation bar!</div>;
+import { DayIcon, GithubIcon, NightIcon } from "../../icons";
+
+const Navigation = () => (
+  <nav className={styles.navigation}>
+    <div className={[styles.navFlex, "container"].join(" ")}>
+      <div className={styles.navFlex}>
+        <img
+          src={ReactLogo}
+          className={styles.navigationMain}
+          alt="The React.js framework logo. Looks like science."
+        />
+        <span>React Angolia</span>
+      </div>
+
+      <div className={[styles.navFlex, styles.pushLeft].join(" ")}>
+        <button className={styles.navigationTOD}>{DayIcon()}</button>
+        <a className={styles.navigationIcon} href="https://github.com/SNVtahoe">
+          {GithubIcon()}
+        </a>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Navigation;
