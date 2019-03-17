5c7666b23689f3ea9059075614b2c76aa8585c11
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-03-17 16:02:15 -0700

    Added Theme Context and Provider logic.

--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,7 +1,42 @@
-import React, { useReducer } from "react";
+// Dependencies
+import React, { createContext, useReducer } from "react";
 
-const GlobalContext = () => {
-  return <></>;
+// Action Type definition
+const TOGGLE_MODE = "TOGGLE_MODE";
+
+// Creating context using React.createContext
+const Theme = createContext();
+
+// Reducer function for useReducer
+const ThemeReducer = (state, action) => {
+  switch (action.type) {
+    case TOGGLE_MODE:
+      return {
+        ...state,
+        toggle: action.payload
+      };
+    default:
+      return state;
+  }
 };
 
-export { GlobalContext };
+// Initial state to feed into useReducer
+// Only need to worry about one variable, as this
+// context provider is only concerned with how
+// the application should look like
+const INITIAL_STATE = {
+  theme: "Day"
+};
+
+// Creating context to be exported
+// grabbing children from incoming props
+// to render incoming functions
+const ThemeContext = ({ children }) => {
+  const [state, dispatch] = useReducer(ThemeReducer, INITIAL_STATE);
+
+  return (
+    <Theme.Provider value={{ state, dispatch }}>{children}</Theme.Provider>
+  );
+};
+
+export { ThemeContext };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { ThemeContext } from "./components/Session";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeContext>
+    <App />
+  </ThemeContext>,
+  document.getElementById("root")
+);
