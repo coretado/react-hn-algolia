ca0d66e909df1ab997671c631452fa768368dc9b
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-17 14:36:29 -0700

    Getting project ready for hosting on GitHub pages.

--- src/components/Post/PostItem/index.js
+++ src/components/Post/PostItem/index.js
@@ -56,23 +56,3 @@ const PostItem = ({
 );
 
 export default PostItem;
-
-// const Post = (props) => {
-//   const { author, created_at_i, num_comments, objectID, points, title, story_title, url } = props.post;
-//   return (
-//     <div className='post-card'>
-//       <a href={url}>{story_title ? story_title : title}</a>
-//       <div className='row'>
-//         <span><a href={`https://news.ycombinator.com/item?id=${objectID}`}>{points ? points : 0} points</a></span>
-//         <span className='leave-a-space'>|</span>
-//         <span><a href={`https://news.ycombinator.com/user?id=${author}`}>{author}</a></span>
-//         <span className='leave-a-space'>|</span>
-//         <span><a href={`https://news.ycombinator.com/item?id=${objectID}`}>{tellTime(created_at_i)}</a></span>
-//         <span className='leave-a-space'>|</span>
-//         <span><a href={`https://news.ycombinator.com/item?id=${objectID}`}>{num_comments} comments</a></span>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Post;
