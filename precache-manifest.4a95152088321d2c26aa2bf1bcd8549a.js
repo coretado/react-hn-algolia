62d195313da90a999d4265fa91068cc707cbf0d4
SNVTahoe <41130062+SNVtahoe@users.noreply.github.com>
2019-04-17 14:37:55 -0700

    Updates

---
+++ precache-manifest.4a95152088321d2c26aa2bf1bcd8549a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ef1cbcfa05495818a9d63927b452cb",
+    "url": "/react-hn-algolia/static/media/ReactLogo.26ef1cbc.svg"
+  },
+  {
+    "revision": "165e9da403c7b3d07181",
+    "url": "/react-hn-algolia/static/js/runtime~main.165e9da4.js"
+  },
+  {
+    "revision": "ae481ad7d907840a8e76",
+    "url": "/react-hn-algolia/static/js/main.ae481ad7.chunk.js"
+  },
+  {
+    "revision": "b4211e13c7d980db6761",
+    "url": "/react-hn-algolia/static/js/1.b4211e13.chunk.js"
+  },
+  {
+    "revision": "ae481ad7d907840a8e76",
+    "url": "/react-hn-algolia/static/css/main.b2a54d06.chunk.css"
+  },
+  {
+    "revision": "c06a3f6c52389f28c5b275225adee666",
+    "url": "/react-hn-algolia/index.html"
+  }
+];
